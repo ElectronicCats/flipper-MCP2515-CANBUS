a900d7eb620e008421bd50c6b8e23ffbe66de30a
xpeqex <carlosalat01@gmail.com>
2024-11-08 12:20:06 -0600

    feat(fmw): icons for general
    icons for general view

--- Canbus_app/draw_functions/drawWidgets.c
+++ Canbus_app/draw_functions/drawWidgets.c
@@ -4,22 +4,18 @@
 void draw_in_development(App* app) {
     widget_reset(app->widget);
 
-    widget_add_string_element(
-        app->widget, 65, 20, AlignCenter, AlignBottom, FontPrimary, "SCENE IN");
+    widget_add_string_multiline_element(
+        app->widget, 65, 40, AlignCenter, AlignBottom, FontPrimary, "SCENE IN\nDEVELOPMENT");
 
-    widget_add_string_element(
-        app->widget, 65, 35, AlignCenter, AlignBottom, FontPrimary, "DEVELOPMENT");
 }
 
 // Draws device not connected
 void draw_device_no_connected(App* app) {
     widget_reset(app->widget);
+    widget_add_icon_element(app->widget, 4, 0, &I_NOC119x38);
+    widget_add_string_multiline_element(
+        app->widget, 65, 60, AlignCenter, AlignBottom, FontPrimary, "DEVICE NOT\nCONNECTED");
 
-    widget_add_string_element(
-        app->widget, 65, 20, AlignCenter, AlignBottom, FontPrimary, "DEVICE NOT");
-
-    widget_add_string_element(
-        app->widget, 65, 35, AlignCenter, AlignBottom, FontPrimary, "CONNECTED");
 }
 
 // draw when a message is not recognized
@@ -27,22 +23,20 @@ void draw_transmition_failure(App* app) {
     widget_reset(app->widget);
 
     widget_add_string_element(
-        app->widget, 65, 20, AlignCenter, AlignBottom, FontPrimary, "TRANSMITION");
+        app->widget, 65, 40, AlignCenter, AlignBottom, FontPrimary, "TRANSMITION\nFAILURE");
 
-    widget_add_string_element(
-        app->widget, 65, 35, AlignCenter, AlignBottom, FontPrimary, "FAILURE");
 }
 
 // draw when a message is send OK
 void draw_send_wrong(App* app) {
     widget_reset(app->widget);
-    widget_add_string_element(
-        app->widget, 65, 20, AlignCenter, AlignCenter, FontPrimary, "MESSAGE SEND ERROR");
+    widget_add_string_multiline_element(
+        app->widget, 65, 40, AlignCenter, AlignCenter, FontPrimary, "ERROER SENDING\nMESSAGE");
 }
 
 // draw when a message is send ok
 void draw_send_ok(App* app) {
     widget_reset(app->widget);
     widget_add_string_element(
-        app->widget, 65, 20, AlignCenter, AlignCenter, FontPrimary, "MESSAGE SEND OK");
+        app->widget, 65, 24, AlignCenter, AlignCenter, FontPrimary, "MESSAGE SEND OK");
 }
